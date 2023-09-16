28ef9d2564ced079a1135a97d9fe947162656b3c
Tobias Knuth <tknuth@users.noreply.github.com>
2023-09-16 21:04:57 +0200

    added expressions.py

---
+++ expressions.py
@@ -0,0 +1,19 @@
+1 + 2.3 == 3.3
+1.1 + 2.2 == 3.3
+2 > 1 == True
+4 > 2 == True
+0 == True
+1 == True
+2 == True
+[] == False
+bool([]) == False
+1 in [1, 2]
+1 in ["1", "2"]
+True in [1, 2]
+1 in [True, False]
+2 in [True, False]
+1 + True
+True + True
+1 + "1"
+1 + ("a" in "abc")
+"1" + "a" in "abc"
