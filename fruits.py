82346a11366d3feb2fc6fd764dfb1c6a6f3b2071
Tobias Knuth <tknuth@users.noreply.github.com>
2024-03-30 18:47:03 +0100

    added people, fruits

---
+++ fruits.py
@@ -0,0 +1,46 @@
+fruits = [
+    {
+        "name": "apple",
+        "price": 50,
+    },
+    {
+        "name": "banana",
+        "price": 90,
+    },
+    {
+        "name": "cherry",
+        "price": 290,
+    },
+    {
+        "name": "grape",
+        "price": 195,
+    },
+    {
+        "name": "kiwi",
+        "price": 100,
+    },
+    {
+        "name": "lemon",
+        "price": 150,
+    },
+    {
+        "name": "mango",
+        "price": 290,
+    },
+    {
+        "name": "pomegranate",
+        "price": 320,
+    },
+    {
+        "name": "orange",
+        "price": 80,
+    },
+    {
+        "name": "peach",
+        "price": 70,
+    },
+    {
+        "name": "pear",
+        "price": 80,
+    },
+]
---
+++ people.py
@@ -0,0 +1,82 @@
+people = [
+    {
+        "name": "Anna",
+        "age": 29,
+        "hobbies": [
+            "reading",
+            "hiking",
+        ],
+    },
+    {
+        "name": "Tim",
+        "age": 35,
+        "hobbies": [
+            "hiking",
+            "swimming",
+        ],
+    },
+    {
+        "name": "Daniel",
+        "age": 24,
+        "hobbies": [
+            "gaming",
+            "reading",
+        ],
+    },
+    {
+        "name": "Jan",
+        "age": 31,
+        "hobbies": [
+            "photography",
+            "hiking",
+        ],
+    },
+    {
+        "name": "Sandra",
+        "age": 28,
+        "hobbies": [
+            "cooking",
+            "reading",
+        ],
+    },
+    {
+        "name": "Jana",
+        "age": 25,
+        "hobbies": [
+            "swimming",
+            "hiking",
+        ],
+    },
+    {
+        "name": "Tom",
+        "age": 33,
+        "hobbies": [
+            "gaming",
+            "photography",
+        ],
+    },
+    {
+        "name": "Lena",
+        "age": 27,
+        "hobbies": [
+            "reading",
+            "swimming",
+        ],
+    },
+    {
+        "name": "Max",
+        "age": 30,
+        "hobbies": [
+            "cycling",
+            "cooking",
+        ],
+    },
+    {
+        "name": "Lisa",
+        "age": 26,
+        "hobbies": [
+            "reading",
+            "hiking",
+        ],
+    },
+]
